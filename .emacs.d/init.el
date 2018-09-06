106b2e220530ed918113311e6ffb954cedf2c7dd
psato <forhighlow@gmail.com>
2018-09-06 17:24:46 +0900

    python-mode-mapのエラー解除のために該当文を含む文を削除

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1059,7 +1059,7 @@
 ;; バッファ全体のコード整形
 ;;(define-key python-mode-map (kbd "C-c F") 'py-autopep8)   
 ;; 選択リジョン内のコード整形
-(define-key python-mode-map (kbd "C-c f") 'py-autopep8-region)
+;;(define-key python-mode-map (kbd "C-c f") 'py-autopep8-region)
 ;; 保存時にバッファ全体を自動整形する
 ;; (add-hook 'before-save-hook 'py-autopep8-before-save)
 
