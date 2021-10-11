4c32ad21b766ffc9d8d4f136b6790cf3c4443f14
sato.t <sato.t@sockets.co.jp>
2021-10-11 15:55:50 +0900

    Update sym link

--- dotfilesLinks.sh
+++ dotfilesLinks.sh
@@ -2,5 +2,6 @@
 ln -s ~/.dotfiles/dotfiles/.zshrc ~/.zshrc 
 ln -s ~/.dotfiles/dotfiles/.emacs.d ~/.emacs.d 
 ln -s ~/.dotfiles/dotfiles/.emacs.d/init.el ~/.emacs.d/init.el
-ln -s ~/.dotfiles/dotfiles/fish/config.fish ~/.config/fish/config.fish
+ln -s ~/.dotfiles/dotfiles/fish ~/.config/fish
 ln -s ~/.dotfiles/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+ln -s ~/.dotfiles/dotfiles/.tmux.conf ~/.tmux.conf
