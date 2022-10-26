9a6c881a78bb3d8451ed05bb2eba6b3ed6ced801
sato.t <sato.t@sockets.co.jp>
2022-10-26 10:05:19 +0900

    Update alias

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,21 @@
+# PATH
+set PATH /usr/local/bin /usr/sbin $PATH
+
 export LSCOLORS=gxfxcxdxbxegedabagacad
 alias ｌｓ='ls'
 alias g='git'
+alias gs='git status'
+alias ｇｓ='git status'
 
 # pyenv
 set -x PYENV_ROOT $HOME/.pyenv
-set -x PATH  $PYENV_ROOT/bin $PATH
+set -x PATH $PYENV_ROOT/shims $PATH
 pyenv init - | source
+
+# git
+export TAG="date +DEPLOYED-%F/%H:%M"
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
+# docker
+alias dls='docker container ls'
